26340d83d46cd43424e0dcc43ea3e5c7851e47fc
RosalieSchaefer <104739382+RosalieSchaefer@users.noreply.github.com>
2022-05-04 21:08:37 +0200

    Add files via upload

---
+++ Widmark-Formel.py
@@ -0,0 +1,30 @@
+# Widmark-Formel Programm
+# von mir
+
+#Überschrift
+print("Dieses Programm berechnet den Blutalkoholwert in Promille")
+
+#Variablen eingeben
+körpergewichttext: str = input("Ihr Körpergewicht in Kilogramm: ")
+anzahlflaschentext: str = input("Wie viele Flaschen Bier haben sie getrunken?: ")
+geschlecht: str = input("Sind sie männlich (m) oder weiblich (w): ")
+
+#texteingaben in Zahlen umwandeln
+körpergewicht: float =float(körpergewichttext)
+anzahlflaschen: float =float(anzahlflaschentext)
+
+#alkoholmasse berechnen
+alkoholmasse: float= anzahlflaschen*0.5*0.5*0.8*100
+print("Alkoholmasse= ",alkoholmasse ," Gramm")
+
+#Reduktionsfaktor bestimmen
+reduktionsfaktor: float=0
+if geschlecht == "m":
+    reduktionsfaktor=0.7
+else:
+    reduktionsfaktor=0.6
+print("geschlecht: ",geschlecht,", das bedeutet einen Reduktionsfaktor von: ", reduktionsfaktor)
+
+#Blutalkohol
+blutalkoholinpromille: float = alkoholmasse/(körpergewicht*reduktionsfaktor)
+print("Ihr Blutalkoholwert in Promille beträgt: ", round(blutalkoholinpromille, 2))
